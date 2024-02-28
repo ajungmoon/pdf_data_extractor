6e0e339a39e12b0d083dbc6e917cf889f4eee1ed
ajungmoon <ajungmoon@gmail.com>
2024-02-27 22:31:46 -0500

    updates

--- file_to_txt.py
+++ file_to_txt.py
@@ -6,6 +6,10 @@ import subprocess
 outdir = './PDF_to_TXT'
 
 # Import a list of file locations (file_locations.txt) to pull PDFs from
+# Use ls > file_locations.txt in commandline if importing all files from a specific folder
+
+# OR alternatively, in commandline of the folder with PDFs, do ==> for i in *.pdf; do pdftotext "$i"; done
+
 with open('file_locations.txt', 'r') as f:
     for count, line in enumerate(f, start=1):
         ext = os.path.splitext(line.strip())[-1].lower()
@@ -13,8 +17,7 @@ with open('file_locations.txt', 'r') as f:
         #       os.path.join(outdir, "file-%02d.txt" % count))
         if ext == '.pdf':
             try:
-                subprocess.check_output(['pdftotext', line.strip(),
-                                         os.path.join(outdir, "file-%02d.txt" % count)])
+                subprocess.check_output(['pdftotext', line.strip()])
             except subprocess.CalledProcessError as e:
                 print(line.strip())
                 print(e.output)
